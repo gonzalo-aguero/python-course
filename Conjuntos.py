7e1caa0f0854b3a4d3e177061d37a4a52f1d4068
Gonzalo Aguero <gonzalo90fa@gmail.com>
2021-09-01 11:22:07 -0300

    sa

--- Conjuntos.py
+++
@@ -1,9 +0,0 @@
-# Conjuntos
-A = {1,2,3,4}
-B = {2,3,5,6}
-C = {3,4,6,7}
-
-print(A | C) # Union de conjuntos
-print(A & C) # Interseccion de conjuntos
-print(A - B) # Elementos que contiene A pero B no.
-print(B ^ C) # Diferencia simetrica (no tiene en cuenta los elementos compartidos)
